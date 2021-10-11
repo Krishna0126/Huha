ac421bcab5481d233873acde9e803d4990a88df8
Krishna0126 <90450835+Krishna0126@users.noreply.github.com>
2021-10-11 12:13:16 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+var array = ['hello','o',123, 2.5];
+console.log(array[3]);
+console.log(array.length);
+array.push("human");
+console.log(array[4]);
+console.log(["Monday","Tuesday"].concat(["Wednesday", "Thursday", "Friday"]));
+array.reverse();
+console.log(array);
